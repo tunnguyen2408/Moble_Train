e8aefa7a8fe976a853ded06e988c39a114c5fcdd
tannv <cuongdt@bizmates.co.jp>
2024-11-28 00:35:11 +0700

    Initial Commit

---
+++ TrainTicket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainTicket
+//
+//  Created by Tũn Nguyễn on 28/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
