755fcd12d4bb41e3b2d19b2abf5bca6ac34520d4
tannv <cuongdt@bizmates.co.jp>
2025-01-03 12:23:18 +0700

    ad

--- TrainTicket/InfoViewController.swift
+++ TrainTicket/InfoViewController.swift
@@ -8,13 +8,32 @@
 import Foundation
 import UIKit
 class InfoViewController: UIViewController {
+    @IBOutlet weak var tvName: UILabel!
+    @IBOutlet weak var tvMail: UILabel!
+    @IBOutlet weak var tvPhone: UILabel!
+    @IBOutlet weak var tvAddress: UILabel!
+    @IBOutlet weak var tvGender: UILabel!
+    @IBOutlet weak var tvDob: UILabel!
     @IBAction func logoutFunc(_ sender: Any) {
         logoutAndRedirectToLogin()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        getInfo()
     }
     
+    
+    
+    private func getInfo() {
+        if let info = CommonUserDefaults.shared.get(forKey: "currentCustomer", type: Customer.self) {
+            tvName.text = "\(info.fname) \(info.lname)"
+            tvMail.text = "Email: \(info.mailid)"
+            tvPhone.text = "SĐT: \(info.phno)"
+            tvAddress.text = "Địa chỉ: \(info.addr)"
+            tvGender.text = "Giới tính: \(info.gender)"
+            tvDob.text = "Ngày sinh: \(info.dob)"
+        }
+    }
     func logoutAndRedirectToLogin() {
         // Xóa thông tin user
         CommonUserDefaults.shared.remove(forKey: "currentCustomer")
